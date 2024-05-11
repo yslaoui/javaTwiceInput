fd0cb71b04b17db5e3e83533ae15d7dec9c86daf
yslaoui <yslaouiyassine@gmail.com>
2024-05-11 19:41:31 +0100

    Subproblem 3: Stop prompting for user input when the the same word has been entered twice

--- UserInterface.java
+++ UserInterface.java
@@ -1,22 +1,30 @@
 package twice;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class UserInterface {
 
     Scanner scanner;
+    ArrayList<String> words;
 
     public UserInterface(Scanner scanner) {
         this.scanner = scanner;
+        this.words = new ArrayList<>();
     }
 
     public void start() {
         String word = "-1";
-        while(!(word.equals("end"))) {
+        while(true) {
             System.out.print("Write a word: ");
             word = this.scanner.nextLine();
+            if (this.words.contains(word)) {
+                System.out.println("You entered this word twice !");
+                break;
+            }
+            words.add(word);
         }
-        System.out.println("You wrote the word end !");
-
     }
 }
+
+
