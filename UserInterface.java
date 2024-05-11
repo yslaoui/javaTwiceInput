6b5e84db3c98df626cc6eceec11e6b8221e8d626
yslaoui <yslaouiyassine@gmail.com>
2024-05-11 19:18:21 +0100

    Subproblem 2: Continuously reading user input until some condition is reached

--- UserInterface.java
+++ UserInterface.java
@@ -11,6 +11,12 @@ public class UserInterface {
     }
 
     public void start() {
-        //
+        String word = "-1";
+        while(!(word.equals("end"))) {
+            System.out.print("Write a word: ");
+            word = this.scanner.nextLine();
+        }
+        System.out.println("You wrote the word end !");
+
     }
 }
