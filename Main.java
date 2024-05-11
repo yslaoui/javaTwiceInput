25ea14684f036236ccdc676e1f5a410244ec69ac
yslaoui <yslaouiyassine@gmail.com>
2024-05-11 19:06:23 +0100

    First commit. Step 1: Implementing the user interface

---
+++ Main.java
@@ -0,0 +1,12 @@
+package twice;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hi");
+        Scanner scanner = new Scanner(System.in);
+        UserInterface userInterface = new UserInterface(scanner);
+
+    }
+}
---
+++ UserInterface.java
@@ -0,0 +1,16 @@
+package twice;
+
+import java.util.Scanner;
+
+public class UserInterface {
+
+    Scanner scanner;
+
+    public UserInterface(Scanner scanner) {
+        this.scanner = scanner;
+    }
+
+    public void start() {
+        //
+    }
+}
